77b34a0c8c2c0cd7628ac2437c1ca6cd62db0232
Cristina Altreche <c.altreche@gmail.com>
2020-08-05 15:50:55 -0400

    Initial commit - server is running

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
-const express = require('express');
+const express = require("express");
 
+const morgan = require("morgan");
+const helmet = require("helmet");
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+server.use(express.json());
+server.use(helmet());
+server.use(logger);
+
+server.get("/", (req, res) => {
+  const nameInsert = req.name ? `${req.name}` : ""
+
+  res.send(`<h2>Let's write some middleware!</h2>
+  <p>Welcome ${nameInsert} to the Lambda Users API</p>`
+  
+  );
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const name = req.headers.name;
+  req.name = name;
+  console.log(`${req.name} made a ${req.method} request to ${req.url}`);
+  next();
+}
 
 module.exports = server;
